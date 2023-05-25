e59bcbd4c20cf9a4ddafc96b6c9d95297bc85f0b
NareshPratista28 <2141720057@student.polinema.ac.id>
2023-05-25 14:45:15 +0700

    Menambahkan Welcome Sementara

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,7 +1,7 @@
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-white elevation-4">
             <!-- Brand Logo -->
-            <a href="{{ route('index') }}" class="brand-link">
+            <a href="{{ route('home') }}" class="brand-link">
                 <img src="{{asset('AdminLTE/dist')}}/img/Group 1.png" alt="Group Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-heavy text-dark">Sahabat Tani</span>
             </a>
