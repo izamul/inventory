38ecbfe6b5dc271eaac49c753660ced003afef64
NareshPratista28 <2141720057@student.polinema.ac.id>
2023-06-02 14:32:13 +0700

    update landing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,7 +40,7 @@
     <!-- ===============================================-->
     <main class="main" id="top">
       <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3 bg-light opacity-85" data-navbar-on-scroll="data-navbar-on-scroll">
-        <div class="container"><a class="navbar-brand" href="index.html"><img class="d-inline-block align-top img-fluid" src="zoufarm/public/assets/img/gallery/logo-icon.png" alt="" width="40" /><span class="text-theme font-monospace fs-4 ps-2">Sahabat Tani</span></a>
+        <div class="container"><a class="navbar-brand" href="{{ route('home') }}"><img class="d-inline-block align-top img-fluid" src="zoufarm/public/assets/img/gallery/logo-icon.png" alt="" width="40" /><span class="text-theme font-monospace fs-4 ps-2">Sahabat Tani</span></a>
           <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
           <div class="collapse navbar-collapse border-top border-lg-0 mt-4 mt-lg-0" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
@@ -69,7 +69,7 @@
           <div class="row align-items-center min-vh-75 min-vh-lg-100">
             <div class="col-md-7 col-lg-6 col-xxl-5 py-6 text-sm-start text-center">
               <h1 class="mt-6 mb-sm-4 fw-semi-bold lh-sm fs-4 fs-lg-5 fs-xl-6">Selamat Datang Di<br class="d-block d-lg-block" />Kios Sahabat Tani</h1>
-              <p class="mb-4 fs-1">Zou provides farmes, ranchers, private foresters, and agricultural producers with online self service applications and educational materials.</p>
+              <p class="mb-4 fs-1">Kami mengundang Anda untuk menjelajahi, mengeksplorasi produk dan layanan kami, serta menghubungi tim kami jika Anda memiliki pertanyaan atau ingin berdiskusi tentang kebutuhan pertanian Anda. Bersama-sama, mari kita menciptakan masa depan pertanian yang berkelanjutan dan menguntungkan untuk semua pihak.</p>
             </div>
           </div>
         </div>
